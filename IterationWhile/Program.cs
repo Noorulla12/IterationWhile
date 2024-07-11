f38d8930f650fe32a5c9433caf855ba4e025b062
rupan <rupan@SSSOLVEM2800>
2024-07-11 14:48:43 +0530

    Add project files.

---
+++ IterationWhile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Iterations1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                /*  Console.Write("Type your name: ");
+                  var input = Console.ReadLine();
+
+                  if (string.IsNullOrEmpty(input))
+                  {
+                      break;
+                  }
+
+                  Console.WriteLine("@Echo : " + input);  */
+    
+
+                Console.Write("Type your name :");
+                var input = Console.ReadLine();
+
+                if(!String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                } 
+
+                break;  
+
+             
+
+
+            }
+        }
+    }
+}
